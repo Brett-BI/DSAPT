e9c9ea91a0608947aab9d8e53a32c11908a300ba
Brett Burley-Inners <bbi@macbookpro.myfiosgateway.com>
2025-01-01 22:06:28 -0500

    Moved more files around. Additional notes for LCA.

--- Problems/LCA_BinaryTree/LowestCommonAncestor.py
+++ Problems/LCA_BinaryTree/LowestCommonAncestor.py
@@ -1,9 +1,33 @@
 # Definition for a binary tree node.
+from typing import Any, Optional
+
+
 class TreeNode:
-    def __init__(self, x):
+    def __init__(self, x: Any, left: Optional['TreeNode'] = None, right: Optional['TreeNode'] = None):
         self.val = x
-        self.left = None
-        self.right = None
+        self.left = left
+        self.right = right
+    
+    # TODO: Add comparison dunder methods so that TreeNodes can be compared to 
+    # other tree nodes.
+
+    # TODO: Add function to spit out text representation of tree graph.
+
+# for [3,5,1,6,2,0,8,null,null,7,4]
+BinaryTree = TreeNode(3)
+BinaryTree.left = TreeNode(5)
+BinaryTree.left.left = TreeNode(6)
+BinaryTree.left.right = TreeNode(2)
+BinaryTree.left.right.left = TreeNode(7)
+BinaryTree.left.right.right = TreeNode(4)
+BinaryTree.right = TreeNode(1)
+BinaryTree.right.left = TreeNode(0)
+BinaryTree.right.right = TreeNode(8)
+
+print(BinaryTree)
+print(BinaryTree.left.val)
+print(BinaryTree.left.left)
+print(BinaryTree.left.right)
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
@@ -34,6 +58,10 @@ class Solution:
         # return the match we have (because a node can be a descendant of itself)
         return l or r
 
+# NOTE: p and q are actually the entire branches, not single tree nodes with no children.
+s = Solution()
+a = s.lowestCommonAncestor(BinaryTree, TreeNode(5), TreeNode(1))
+print(a)
 
 # Complexity
 # Based on the size of the tree
