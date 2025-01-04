c12ab45bd016ca3270020e9c9ae7e0580a06263b
Brett Burley-Inners <bbi@bretts-mbp.myfiosgateway.com>
2025-01-03 19:16:31 -0500

    Added LC problem 339

---
+++ Problems/339_NestedWeightListSum/code.py
@@ -0,0 +1,72 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        # start the total sum at 0; use nonlocal to access the variable in this scope
+        sum: int = 0
+
+        # recursive function
+        def calculateSum(item: NestedInteger, depth: int):
+            nonlocal sum # initialize parent scope's sum here
+
+            # Based on NestedInteger class above, the type of the item/element
+            # needs to be checked. The item can only be two things: integer or List.
+            if item.isInteger():
+                # increment sum by item * depth
+                sum += item.getInteger() * depth
+            else:
+                # Loop through the list, use recursion to evaluate each function.
+                # Each loop with either add to sum variable or it will loop through
+                # the next nested List
+                for l in item.getList():
+                    calculateSum(l, depth + 1)
+
+        # depth can be 1 to start because of constraints
+        for nl in nestedList:
+            calculateSum(nl, 1)
+
+        return sum
+
+# T: O(n); this is only evaluating each element once even though there are two for loops
+# S: O(n + 1) => O(n)
